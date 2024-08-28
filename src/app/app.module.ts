82c4e4f4f0b863ece5490fc6f942724349be6a61
Carlos Paez <carlos31298@gmail.com>
2024-08-28 13:01:38 -0700

    Dbz App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DbzModule } from "./dbz/dbz.module";
 
 @NgModule({
   declarations: [
@@ -15,8 +16,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
-  ],
+    HeroesModule,
+    DbzModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/charactes.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [];
+
+  @Output()
+  public onDeleteId: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    if(!id) return;
+
+    this.onDeleteId.emit(id);
+  }
+}
